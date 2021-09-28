72af90d299874f8f77d306d07d10fd99a55d23d1
Erick <etyvaldirmc@gmail.com>
2021-09-28 18:21:00 -0400

    asignacion de unidades finalizado

--- sistema-app/modules/productos/asignar_unidad.php
+++ sistema-app/modules/productos/asignar_unidad.php
@@ -10,20 +10,66 @@
 // Verifica si es una peticion post
 if (is_ajax() && is_post()) {
 	// Verifica la existencia de los datos enviados
-	if (isset($_POST['unidad']) && isset($_POST['sigla']) && isset($_POST['descripcion']) && isset($_POST['cantidad']) && isset($_POST['precio']) ) {
+	if (isset($_POST['id_producto']) && isset($_POST['unidad']) && isset($_POST['sigla']) && isset($_POST['descripcion']) && isset($_POST['cantidad']) && isset($_POST['precio']) ) {
 		// Importa la libreria para convertir el numero a letra
 		require_once libraries . '/numbertoletter-class/NumberToLetterConverter.php';
 
 		// Obtiene los datos del producto
-		$unidad = trim($_POST['unidad']);
+		$id_producto = trim($_POST['id_producto']);
+		$nombre_unidad = trim($_POST['unidad']);
 		$sigla= trim($_POST['sigla']);
 		$descripcion = trim($_POST['descripcion']);
-        $cantiadad = trim($_POST['cantiadad']);
+        $cantidad = trim($_POST['cantidad']);
         $precio = trim($_POST['precio']);
 
-        
+
+		//obtiene el producto
+        $unidad = $db->select('*')->from('inv_unidades')->where(array('unidad' => $nombre_unidad, 'sigla' => $sigla))->fetch_first();
+        if(!$unidad){
+			// Instancia nueva unidad
+            $new = array(
+                'unidad' => $nombre_unidad,
+                'sigla' => $sigla,
+				'descripcion'=>$descripcion
+            );
+            $unidad_id = $db->insert('inv_unidades',$new);
+        }else{
+			$unidad_id = $unidad['id_unidad'];
+		}
+
+		// Instancia la asignacion
+		$asignacion= array(
+			'producto_id' => $id_producto,
+			'unidad_id' => $unidad_id,
+			'cantidad_unidad' => $cantidad,
+			'otro_precio' => $precio,
+			'fecha_asignacion' => date('Y-m-d'),
+		);
+
+		// Guarda la informacion
+		$id_asignacion = $db->insert('inv_asignaciones', $asignacion);
+		
+		// Instancia el precio
+		$precio = array(
+			'precio' => $precio,
+			'fecha_registro' => date('Y-m-d'),
+			'hora_registro' => date('H:i:s'),
+			'asignacion_id' => $id_asignacion,
+			'producto_id' => $id_producto,
+			'empleado_id' => $_user['persona_id'],
+		);
+
+		// Guarda la informacion
+		$id_precio = $db->insert('inv_precios', $precio);
 
 
+
+		// Instancia la variable de notificacion
+		$respuesta = array(
+			'alert' => 'success',
+			'title' => 'Asignación satisfactoria!',
+			'message' => 'El registro se guardó correctamente.'
+		);
 
 		// Envia respuesta
 		echo json_encode($respuesta);
