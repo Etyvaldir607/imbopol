33cc0ccbf0d6018ec134cc536e721c600263cece
Erick <etyvaldirmc@gmail.com>
2021-10-11 16:07:30 -0400

    correccion - llamada a la funcion

--- sistema-app/modules/manuales/crear.php
+++ sistema-app/modules/manuales/crear.php
@@ -924,7 +924,7 @@ function guardar_nota() {
 			}, {
 				type: 'success'
 			});
-			//imprimir_nota(venta);
+			generar_pdf_venta(venta.id_egreso);
 		} else {
 			$('#loader').fadeOut(100);
 			$.notify({
@@ -942,9 +942,18 @@ function guardar_nota() {
 		});
 	}).always(function (){
 		$('#formulario :reset').trigger('click');
+		window.location.reload();
 	});
 }
 
+
+/**  inicia la peticion para generar pdf de la compra */
+function generar_pdf_venta(id) {
+	$id_egreso = parseInt(id);
+	url = '?/manuales/imprimir/' + $id_egreso;
+	//console.log(url)
+	window.open(url,'_blank');
+}
 
 function actualizar(elemento) {
 	var $elemento = $(elemento), actualizar;
