27e57e0068d070fe6db50873872676929c34ed1b
Erick <etyvaldirmc@gmail.com>
2021-10-11 16:29:26 -0400

    agregando parametros de venta

--- sistema-app/modules/electronicas/guardar.php
+++ sistema-app/modules/electronicas/guardar.php
@@ -102,7 +102,7 @@ if (is_ajax() && is_post()) {
 			// Recorre los productos
 			foreach ($productos as $nro => $elemento) {
                 // recupera unidades
-                $id_unidad = $db->select('id_unidad')->from('inv_unidades')->where('unidad',$unidad[$nro])->fetch_first()['id_unidad'];
+                // $id_unidad = $db->select('id_unidad')->from('inv_unidades')->where('unidad',$unidad[$nro])->fetch_first()['id_unidad'];
                 /*
                 $id_unidade=$db->select('*')->from('inv_asignaciones a')->join('inv_unidades u','a.unidad_id=u.id_unidad')->where(array('u.unidad' => $unidad[$nro], 'a.producto_id' => $productos[$nro]))->fetch_first();
                 if($id_unidade){
@@ -114,6 +114,11 @@ if (is_ajax() && is_post()) {
                     $cantidad = $cantidades[$nro];
                 }
                 */
+
+				// recupera unidades
+				$id_unidad = $db->select('id_unidad')->from('inv_unidades')->where('unidad',$unidad[$nro])->fetch_first()['id_unidad'];
+				$id_asignacion = $db->select('id_asignacion')->from('inv_asignaciones')->where(array('unidad_id'=>$id_unidad, 'estado' =>'a'))->fetch_first()['id_asignacion'];
+
 				// Forma el detalle
 				$detalle = array(
                     'precio' => (isset($precios[$nro])) ? $precios[$nro]: 0,
@@ -122,7 +127,8 @@ if (is_ajax() && is_post()) {
                     'descuento' =>(isset($descuentos[$nro])) ? $descuentos[$nro]: 0,
 					'fecha_vencimiento' => (isset($fechas[$nro])) ? $fechas[$nro]: 0,
                     'producto_id' => $productos[$nro],
-					'egreso_id' => $egreso_id
+					'egreso_id' => $egreso_id,
+					'asignacion_id'=>$id_asignacion
 				);
 
 				// Genera los subtotales
@@ -137,6 +143,7 @@ if (is_ajax() && is_post()) {
 			
 			// Instancia la respuesta
 				$respuesta = array(
+					'id_egreso'=>$egreso_id,
 					'papel_ancho' => 10,
 					'papel_alto' => 25,
 					'papel_limite' => 576,
